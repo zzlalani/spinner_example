1c2f9349d5d260933d260ebe7843f216ead2b58b
Computer Lab Admin <admin@jinnah.edu>
2017-11-04 11:04:40 +0500

    with custom http

--- app/src/main/java/com/zeeshanlalani/spinner_example/MainActivity.java
+++ app/src/main/java/com/zeeshanlalani/spinner_example/MainActivity.java
@@ -13,7 +13,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,29 +60,38 @@ public class MainActivity extends AppCompatActivity {
 
     class GetData extends AsyncTask<Void, Void, Void> {
 
-        JSONObject data;
-
-
         @Override
         protected Void doInBackground(Void... voids) {
-            String url = "https://my-json-server.typicode.com/zzlalani/data/db";
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url(url)
-                    .build();
-
             try {
-                Response response = client.newCall(request).execute();
-                data = new JSONObject(response.body().string());
+                URL url = new URL("https://my-json-server.typicode.com/zzlalani/data/db");
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
 
+                con.setReadTimeout(10000);
+                con.setConnectTimeout(10000);
 
+                InputStream in = con.getInputStream();
+                con.connect();
+                BufferedReader br = null;
+                if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
 
-                JSONArray courses = data.getJSONArray("courses");
+                } else {
+                    br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                }
+
+                JSONObject resp = bufferToJson(br);
+
+                JSONArray courses = resp.getJSONArray("courses");
                 for ( int i = 0; i < courses.length(); i++ ) {
                     JSONObject courseItem = courses.getJSONObject(i);
                     list.add(courseItem.getString("title"));
                 }
 
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (JSONException e) {
@@ -91,6 +107,26 @@ public class MainActivity extends AppCompatActivity {
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item, list);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinner.setAdapter(adapter);
+        }
+
+        JSONObject bufferToJson (BufferedReader br) {
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            try {
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+                JSONObject json = new JSONObject(sb.toString());
+                return json;
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
         }
     }
 }
